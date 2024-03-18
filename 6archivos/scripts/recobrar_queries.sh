ec3b8004fefdc19283b2337275e9a6d84fa24220
Richard Teran <richard_teran@contraloria.gob.bo>
2024-03-18 10:20:30 -0400

    fix: correccion de errores

--- 6archivos/scripts/recobrar_queries.sh
+++ 6archivos/scripts/recobrar_queries.sh
@@ -1,15 +1,54 @@
 #!/bin/bash
 
-# Directorio de origen
-directorio_origen="/home/xixay/Documentos/Richard/ayuda-memoria/6archivos/querys/scripts_dbeaver"
+# Función para obtener el directorio de inicio del usuario
+get_home_directory() {
+    if [ -n "$HOME" ]; then
+        echo "$HOME"
+    elif [ -n "$USER" ]; then
+        echo "/home/$USER"
+    else
+        echo "Error: No se puede determinar el directorio de inicio del usuario."
+        exit 1
+    fi
+}
 
-# Directorio de destino
-directorio_destino="/home/xixay/snap/dbeaver-ce/288/.local/share/DBeaverData/workspace6/General/Scripts/"
+# Función para obtener el nombre del hostname
+get_hostname() {
+    hostname=$(hostname)
+    echo "$hostname"
+}
+
+# Mostrar el nombre del hostname
+echo "Nombre del hostname: $(get_hostname)"
+
+# Detectar el entorno basado en el nombre de host
+case "$(get_hostname)" in
+    "OC-SGSIR-PC20L")
+        echo "Detectado entorno de trabajo: Trabajo"
+        directorio_origen="$(get_home_directory)/Documentos/richard/ayuda-memoria/6archivos/querys/scripts_dbeaver"
+        directorio_destino="$(get_home_directory)/.local/share/DBeaverData/workspace6/General/Scripts/"
+        ;;
+    "nombre-del-equipo-casa")
+        echo "Detectado entorno de trabajo: Casa"
+        directorio_origen="/home/xixay/Documentos/Richard/ayuda-memoria/6archivos/querys/scripts_dbeaver"
+        directorio_destino="/home/xixay/snap/dbeaver-ce/288/.local/share/DBeaverData/workspace6/General/Scripts/"
+        ;;
+    *)
+        echo "Error: No se puede determinar el entorno de trabajo."
+        exit 1
+        ;;
+esac
+
+# Mostrar los directorios de origen y destino
+echo "Directorio de origen: $directorio_origen"
+echo "Directorio de destino: $directorio_destino"
 
 # Borrar contenido del directorio de destino si existe
+echo "Borrando contenido del directorio de destino..."
 rm -rf "$directorio_destino"/*
 
 # Copiar los archivos de scripts desde el directorio de origen al directorio de destino
+echo "Copiando archivos desde el directorio de origen al directorio de destino..."
 cp -r "$directorio_origen"/* "$directorio_destino"
 
 # Verificar si se copiaron los archivos exitosamente
