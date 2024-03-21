5c95ec5c34a78217b61ba57a24d1ca045a7c662a
Richard Teran <richard_teran@contraloria.gob.bo>
2024-03-21 15:26:24 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Sispoa_Persona_Interina.sql
+++ 6archivos/querys/Sispoa_Persona_Interina.sql
@@ -4,7 +4,7 @@
 	;
 --###  --- CARGOS ITEM PERSONA
 	select cip.* from estructura_organizacional.cargos_items_persona cip where cip.cip_codigo in (126, 129) order by cip.cip_codigo asc
-	select * from estructura_organizacional.cargos_items ci where ci.aun_codigo in (1) order by ci.cit_codigo desc -- 602
+	select * from estructura_organizacional.cargos_items ci where ci.aun_codigo in (2) order by ci.cit_codigo desc -- 602
 	select * from estructura_organizacional.cargos_items_persona cip where cip.cit_codigo in (235,602) order by cip.cip_codigo desc -- 587
 	select cip.*
 	from estructura_organizacional.areas_unidades au
