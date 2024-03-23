c2ad4fe137592d923c9b859d8066420b3a55a5ef
rteran <richard_teran@contraloria.gob.bo>
2024-03-22 22:27:19 -0400

    Actualizar scripts de DBeaver

---
+++ 6archivos/querys/Script_informe_recomendaciones_casa.sql
@@ -0,0 +1,7 @@
+--###  --- INFORME RECOMENDACIONES
+	select *from ejecucion_actividades.informe_recomendaciones ir where ir.ire_estado in (1) order by ir.fecha_registro desc
+	select *from ejecucion_actividades.informe_recomendaciones_seguimientos irs
+	select *from parametricas.estados_informe_recomendaciones eir
+	select * from ejecucion_actividades.informes i where i.inf_codigo in (17)
+	select * from ejecucion_actividades.recomendaciones_inicios_seguimientos
+	;
