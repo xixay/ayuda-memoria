9071403a3cc5b68c59fc3b283aca45b492600348
Richard Teran <richard_teran@contraloria.gob.bo>
2024-03-25 10:51:48 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script_informe_recomendaciones.sql
+++ 6archivos/querys/Script_informe_recomendaciones.sql
@@ -1,9 +1,10 @@
 --###  --- INFORME RECOMENDACIONES
-	select *from ejecucion_actividades.informe_recomendaciones ir where ir.ire_estado in (1) order by ir.fecha_registro desc
-	select *from ejecucion_actividades.informe_recomendaciones_seguimientos irs where irs.irs_estado in (1) order by irs.fecha_registro desc 
-	select *from parametricas.estados_informe_recomendaciones eir
+	select * from ejecucion_actividades.informe_recomendaciones ir where ir.ire_estado in (1) order by ir.fecha_registro desc
+	select * from ejecucion_actividades.informe_recomendaciones_seguimientos irs where irs.irs_estado in (1) order by irs.fecha_registro desc 
+	select * from parametricas.estados_informe_recomendaciones eir
 	select * from ejecucion_actividades.informes i where i.inf_codigo in (17)
-	select * from ejecucion_actividades.recomendaciones_inicios_seguimientos
+	select * from ejecucion_actividades.recomendaciones_inicios_seguimientos ris
+	select * from ejecucion_actividades.inicios_actividades ia 
 	;
 --###  --- INICIO ACT POA - AMPLIACION JUSTIFICACION - CAMBIO GLOBAL DE ESTADOS, POR ROL
 	select iap.iap_codigo,iap.iac_codigo,iap.iap_observaciones ,iap.ges_codigo ,iap.iap_estado  from ejecucion_actividades.inicio_actividad_poa iap where iap.iap_codigo in (140)
