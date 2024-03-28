2013e76c86a15753a09c196ab30d04b006826f60
Richard Teran <richard_teran@contraloria.gob.bo>
2024-03-28 17:24:33 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script_informe_recomendaciones.sql
+++ 6archivos/querys/Script_informe_recomendaciones.sql
@@ -4,10 +4,49 @@
 	select * from parametricas.estados_informe_recomendaciones eir
 	select * from ejecucion_actividades.informes i where i.inf_codigo in (17)
 	select * from ejecucion_actividades.recomendaciones_inicios_seguimientos ris
+	-- tiene tabla migrada
 	select * from ejecucion_actividades.inicios_actividades ia where ia.iac_codigo in (141)
-	select * from ejecucion_actividades.inicio_actividad_informe iai where iai.iac_codigo in (141)
+	select * from ejecucion_actividades.inicio_actividad_informe iai where iai.iac_codigo in (141)--inf_codigo=4
 	select * from ejecucion_actividades.inicio_actividad_poa iap where iap.act_codigo in (860)
-	select * from ejecucion_actividades.actividad_migrada_conaud amc 
+	--- migrada existe
+		--- obt inf codigo//res.- 4
+		SELECT iai.inf_codigo
+  		FROM ejecucion_actividades.inicio_actividad_informe iai
+      	WHERE iai.iac_codigo IN (141)
+        --- infomes//res.- 140
+      	SELECT i.iac_codigo
+        FROM ejecucion_actividades.informes i
+        WHERE inf_codigo IN (4)
+    	--- comb//res.- 140
+      	SELECT i.iac_codigo
+        FROM ejecucion_actividades.informes i
+        WHERE inf_codigo IN (
+          SELECT iai.inf_codigo
+          FROM ejecucion_actividades.inicio_actividad_informe iai
+          WHERE iai.iac_codigo IN (141)
+        )
+	select * from ejecucion_actividades.actividad_migrada_conaud amc where amc.iac_codigo in (140)
+	-- no tiene tabla migrada
+	select * from ejecucion_actividades.inicios_actividades ia where ia.iac_codigo in (249)
+	select * from ejecucion_actividades.inicio_actividad_informe iai where iai.iac_codigo in (249)--inf_codigo=17
+	select * from ejecucion_actividades.actividad_migrada_conaud amc where amc.iac_codigo in (245)
+	--- migrada no existe
+		--- obt inf codigo//res.- 17
+		SELECT iai.inf_codigo
+  		FROM ejecucion_actividades.inicio_actividad_informe iai
+      	WHERE iai.iac_codigo IN (249)
+        --- infomes//res.- 245
+      	SELECT i.iac_codigo
+        FROM ejecucion_actividades.informes i
+        WHERE inf_codigo IN (17)
+    	--- comb//res.- 140
+      	SELECT i.iac_codigo
+        FROM ejecucion_actividades.informes i
+        WHERE inf_codigo IN (
+          SELECT iai.inf_codigo
+          FROM ejecucion_actividades.inicio_actividad_informe iai
+          WHERE iai.iac_codigo IN (249)
+        )
 	;
 --###  --- INICIO ACT POA - AMPLIACION JUSTIFICACION - CAMBIO GLOBAL DE ESTADOS, POR ROL
 	select iap.iap_codigo,iap.iac_codigo,iap.iap_observaciones ,iap.ges_codigo ,iap.iap_estado  from ejecucion_actividades.inicio_actividad_poa iap where iap.iap_codigo in (140)
