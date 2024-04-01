ee1f64ffed833279001a4848e0c0a4bc162beab2
Richard Teran <richard_teran@contraloria.gob.bo>
2024-04-01 10:15:20 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script_informe_recomendaciones.sql
+++ 6archivos/querys/Script_informe_recomendaciones.sql
@@ -47,6 +47,60 @@
           FROM ejecucion_actividades.inicio_actividad_informe iai
           WHERE iai.iac_codigo IN (249)
         )
+    select * from estructura_poa.actividades a where a.act_codigo in (860)
+    select * from ejecucion_actividades.inicios_actividades ia 
+	 SELECT
+	          t.iai_codigo,
+	          t.iai_descripcion,
+	          t.inf_codigo,
+	          inf.inf_nombre,
+	          t.iac_codigo,
+	          ia.iac_codigo_control,
+	          t.iai_estado,
+	          e.est_color,
+	          e.est_nombre AS iai_estado_descripcion,
+	          TO_CHAR(t.fecha_registro, 'HH24:MI am dd/mm/yyyy') as fecha_registro
+	    FROM	ejecucion_actividades.inicio_actividad_informe t
+	          LEFT JOIN parametricas.estados e ON e.est_codigo = t.iai_estado
+	          LEFT JOIN ejecucion_actividades.informes inf ON t.inf_codigo = inf.inf_codigo
+	          LEFT JOIN ejecucion_actividades.inicios_actividades ia ON t.iac_codigo = ia.iac_codigo
+	    WHERE	t.inf_codigo in (17)
+	    ORDER BY t.fecha_registro desc
+	    --###############
+	 	select t.iac_codigo
+	    FROM	ejecucion_actividades.inicio_actividad_informe t
+	    WHERE	t.inf_codigo in (17)--iac_codigo=249
+	    
+	   	select t.iac_codigo_control	
+	    FROM	ejecucion_actividades.inicios_actividades t
+	    WHERE	t.iac_codigo in (249)
+	    
+	 	select *
+	    FROM	ejecucion_actividades.inicio_actividad_poa t
+	    WHERE	t.iac_codigo in (249)--ac_codigo=864
+	    
+	 	select a.act_descripcion 
+	 	from estructura_poa.actividades a 
+	 	where a.act_codigo in (864)--act_descripcion=GOBIERNO AUTÓNOMO DEPARTAMENTAL DE ORURO
+	 	
+	 	select * 
+	 	from ejecucion_actividades.informes i 
+	 	where i.inf_codigo in (17)--inf_nombre
+	 	
+	 	select *
+	 	from parametricas.tipos_trabajos tt 
+	 	where tt.ttr_codigo in (12)
+	 	
+	 	--#################
+	 	select t.inf_codigo, t.iac_codigo, ia.iac_codigo_control,tt.ttr_descripcion, ia.ttr_codigo ,iap.act_codigo, a.act_descripcion , i2.inf_nombre
+	    FROM	ejecucion_actividades.inicio_actividad_informe t
+	    left join ejecucion_actividades.inicios_actividades ia on ia.iac_codigo = t.iac_codigo
+	    left join parametricas.tipos_trabajos tt on tt.ttr_codigo = ia.ttr_codigo 
+	    left join ejecucion_actividades.inicio_actividad_poa iap on iap.iac_codigo = t.iac_codigo
+	    left join estructura_poa.actividades a  on a.act_codigo = iap.act_codigo
+	    left join ejecucion_actividades.informes i2 on i2.inf_codigo = t.inf_codigo
+	    WHERE	t.inf_codigo in (17)--iac_codigo=249
+
 	;
 --###  --- INICIO ACT POA - AMPLIACION JUSTIFICACION - CAMBIO GLOBAL DE ESTADOS, POR ROL
 	select iap.iap_codigo,iap.iac_codigo,iap.iap_observaciones ,iap.ges_codigo ,iap.iap_estado  from ejecucion_actividades.inicio_actividad_poa iap where iap.iap_codigo in (140)
