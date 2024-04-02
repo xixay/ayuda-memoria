cecb8b91e3b3ad1e03a8a360ed11e66dba04a738
Richard Teran <richard_teran@contraloria.gob.bo>
2024-04-02 19:19:29 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script_informe_recomendaciones.sql
+++ 6archivos/querys/Script_informe_recomendaciones.sql
@@ -1,129 +1,136 @@
 --###  --- INFORME RECOMENDACIONES
 	select * from ejecucion_actividades.informe_recomendaciones ir where ir.ire_estado in (1) order by ir.fecha_registro desc
-	select * from ejecucion_actividades.informe_recomendaciones_seguimientos irs where irs.irs_estado in (1) order by irs.fecha_registro desc 
+	select * from ejecucion_actividades.informe_recomendaciones_seguimientos irs where irs.irs_estado in (1) and irs.ire_codigo in (1) order by irs.fecha_registro desc 
 	select * from parametricas.estados_informe_recomendaciones eir
+	select *
+    FROM	ejecucion_actividades.inicio_actividad_informe t
+    WHERE	t.iac_codigo in (249)--inf_codigo=17
 	select * from ejecucion_actividades.informes i where i.inf_codigo in (17)
-	select * from ejecucion_actividades.recomendaciones_inicios_seguimientos ris
+	select * from ejecucion_actividades.recomendaciones_inicios_seguimientos ris where ris.iac_codigo in (249)
 	-- tiene tabla migrada
 	select * from ejecucion_actividades.inicios_actividades ia where ia.iac_codigo in (141)
 	select * from ejecucion_actividades.inicio_actividad_informe iai where iai.iac_codigo in (141)--inf_codigo=4
 	select * from ejecucion_actividades.inicio_actividad_poa iap where iap.act_codigo in (860)
-	--- migrada existe
-		--- obt inf codigo//res.- 4
-		SELECT iai.inf_codigo
-  		FROM ejecucion_actividades.inicio_actividad_informe iai
-      	WHERE iai.iac_codigo IN (141)
-        --- infomes//res.- 140
-      	SELECT i.iac_codigo
-        FROM ejecucion_actividades.informes i
-        WHERE inf_codigo IN (4)
-    	--- comb//res.- 140
-      	SELECT i.iac_codigo
-        FROM ejecucion_actividades.informes i
-        WHERE inf_codigo IN (
-          SELECT iai.inf_codigo
-          FROM ejecucion_actividades.inicio_actividad_informe iai
-          WHERE iai.iac_codigo IN (141)
-        )
+	SELECT 
+      t.ris_codigo,
+      t.iac_codigo,
+      t.ire_codigo,
+      ia.iac_correlativo,
+      ia.iac_codigo_control,
+      ia.iac_objeto,
+      ia.iac_objetivo,
+      ia.iac_alcance,
+      ia.iac_observaciones,
+      ir.ire_descripcion,
+      ir.inf_codigo,
+      irs.irs_aceptacion,
+      irs.irs_justificacion,
+      irs.irs_fecha_inicio,
+      irs.irs_fecha_fin,
+      eir.eir_codigo,
+      eir.eir_nombre,
+      t.ris_estado,
+      e.est_color, 
+      e.est_nombre AS ris_estado_descripcion,
+      TO_CHAR(t.fecha_registro, 'HH24:MI am dd/mm/yyyy') as fecha_registro  
+    FROM ejecucion_actividades.recomendaciones_inicios_seguimientos t
+    LEFT JOIN ejecucion_actividades.inicios_actividades ia ON ia.iac_codigo = t.iac_codigo
+    LEFT JOIN ejecucion_actividades.informe_recomendaciones ir ON ir.ire_codigo = t.ire_codigo
+    LEFT JOIN ejecucion_actividades.informe_recomendaciones_seguimientos irs ON irs.ire_codigo = ir.ire_codigo
+    LEFT JOIN parametricas.estados_informe_recomendaciones eir ON eir.eir_codigo = irs.eir_codigo
+    LEFT JOIN parametricas.estados e ON e.est_codigo = t.ris_estado
+    where t.iac_codigo in (249)
+;
+---### migrada existe
+	--- obt inf codigo//res.- 4
+	SELECT iai.inf_codigo
+	FROM ejecucion_actividades.inicio_actividad_informe iai
+  	WHERE iai.iac_codigo IN (141)
+    --- infomes//res.- 140
+  	SELECT i.iac_codigo
+    FROM ejecucion_actividades.informes i
+    WHERE inf_codigo IN (4)
+	--- comb//res.- 140
+  	SELECT i.iac_codigo
+    FROM ejecucion_actividades.informes i
+    WHERE inf_codigo IN (
+      SELECT iai.inf_codigo
+      FROM ejecucion_actividades.inicio_actividad_informe iai
+      WHERE iai.iac_codigo IN (141)
+    )
 	select * from ejecucion_actividades.actividad_migrada_conaud amc where amc.iac_codigo in (140)
 	-- no tiene tabla migrada
 	select * from ejecucion_actividades.inicios_actividades ia where ia.iac_codigo in (249)
 	select * from ejecucion_actividades.inicio_actividad_informe iai where iai.iac_codigo in (249)--inf_codigo=17
 	select * from ejecucion_actividades.actividad_migrada_conaud amc where amc.iac_codigo in (245)
-	--- migrada no existe
-		--- obt inf codigo//res.- 17
-		SELECT iai.inf_codigo
-  		FROM ejecucion_actividades.inicio_actividad_informe iai
-      	WHERE iai.iac_codigo IN (249)
-        --- infomes//res.- 245
-      	SELECT i.iac_codigo
-        FROM ejecucion_actividades.informes i
-        WHERE inf_codigo IN (17)
-    	--- comb//res.- 140
-      	SELECT i.iac_codigo
-        FROM ejecucion_actividades.informes i
-        WHERE inf_codigo IN (
-          SELECT iai.inf_codigo
-          FROM ejecucion_actividades.inicio_actividad_informe iai
-          WHERE iai.iac_codigo IN (249)
-        )
-    select * from estructura_poa.actividades a where a.act_codigo in (860)
-    select * from ejecucion_actividades.inicios_actividades ia 
-	 SELECT
-	          t.iai_codigo,
-	          t.iai_descripcion,
-	          t.inf_codigo,
-	          inf.inf_nombre,
-	          t.iac_codigo,
-	          ia.iac_codigo_control,
-	          t.iai_estado,
-	          e.est_color,
-	          e.est_nombre AS iai_estado_descripcion,
-	          TO_CHAR(t.fecha_registro, 'HH24:MI am dd/mm/yyyy') as fecha_registro
-	    FROM	ejecucion_actividades.inicio_actividad_informe t
-	          LEFT JOIN parametricas.estados e ON e.est_codigo = t.iai_estado
-	          LEFT JOIN ejecucion_actividades.informes inf ON t.inf_codigo = inf.inf_codigo
-	          LEFT JOIN ejecucion_actividades.inicios_actividades ia ON t.iac_codigo = ia.iac_codigo
-	    WHERE	t.inf_codigo in (17)
-	    ORDER BY t.fecha_registro desc
-	    --############### codigo_poa=500.2002.119.3.24   http://172.16.75.100:3002/conaud/inicios-actividades?ges_codigo=2&act_codigo=864
-	 	select *
-	    FROM	ejecucion_actividades.inicio_actividad_informe t
-	    WHERE	t.iac_codigo in (249)--inf_codigo=17
-	    
-	   	select *
-	    FROM	ejecucion_actividades.inicios_actividades t
-	    WHERE	t.iac_codigo in (249)--iac_codigo_control=EBGP120M24F2
-	    
-	 	select *
-	    FROM	ejecucion_actividades.inicio_actividad_poa t
-	    WHERE	t.iac_codigo in (249)--act_codigo=864
-	    
-	 	select a.act_descripcion 
-	 	from estructura_poa.actividades a 
-	 	where a.act_codigo in (864)--act_descripcion=GOBIERNO AUTÓNOMO DEPARTAMENTAL DE ORURO
-	 	
-	 	select * 
-	 	from ejecucion_actividades.informes i 
-	 	where i.inf_codigo in (17)--inf_nombre
-	 	
-	 	select *
-	 	from parametricas.tipos_trabajos tt 
-	 	where tt.ttr_codigo in (12)
-	 	
-	 	select ie.eev_codigo  
-	 	from ejecucion_actividades.informes_estados ie  
-	 	where ie.inf_codigo in (17)--no hay nada
-	 	order by ie.fecha_registro desc
-	 	limit 1
-	 	
-	 	select ee.eev_sigla 
-	 	from parametricas.estados_evolucion ee 
-	 	where ee.eev_codigo in (38)
-	 	
-	 	SELECT * 
-	 	FROM ejecucion_actividades.informe_recomendaciones ir
-	 	where ir.inf_codigo in (17)
-	 	
-	 	SELECT COUNT(*) as nrecom 
-	 	FROM ejecucion_actividades.informe_recomendaciones ir
-	 	where ir.ire_estado in (1) AND ir.inf_codigo in (17)
-	 	
-	 	--#################
-	 	select t.inf_codigo, t.iac_codigo,ia.iac_fecha_inicio, ia.iac_fecha_fin, ia.iac_dias_habiles, ia.iac_dias_calendario, g.ges_anio, ia.iac_codigo_control, tt.ttr_sigla, tt.ttr_descripcion, ia.ttr_codigo , iap.act_codigo, a.act_numero , a.act_descripcion , i2.inf_codigo_control , i2.inf_nombre, ee.eev_sigla
-	    FROM	ejecucion_actividades.inicio_actividad_informe t
-	    left join ejecucion_actividades.inicios_actividades ia on ia.iac_codigo = t.iac_codigo
-	    left join parametricas.tipos_trabajos tt on tt.ttr_codigo = ia.ttr_codigo
-	    left join parametricas.gestiones g on g.ges_codigo = ia.ges_codigo
-	    left join ejecucion_actividades.inicio_actividad_poa iap on iap.iac_codigo = t.iac_codigo
-	    left join estructura_poa.actividades a  on a.act_codigo = iap.act_codigo
-	    left join ejecucion_actividades.informes i2 on i2.inf_codigo = t.inf_codigo
-	    left join ejecucion_actividades.informes_estados ie on ie.inf_codigo = t.inf_codigo
-	    left join parametricas.estados_evolucion ee on ee.eev_codigo = ie.eev_codigo
-	    WHERE	t.iac_codigo in (249)--iac_codigo=141
-	 	order by ie.fecha_registro desc
-	 	limit 1
-
+;
+--- migrada no existe
+	--- obt inf codigo//res.- 17
+	SELECT iai.inf_codigo
+	FROM ejecucion_actividades.inicio_actividad_informe iai
+  	WHERE iai.iac_codigo IN (249)
+    --- infomes//res.- 245
+  	SELECT i.iac_codigo
+    FROM ejecucion_actividades.informes i
+    WHERE inf_codigo IN (17)
+	--- comb//res.- 140
+  	SELECT i.iac_codigo
+    FROM ejecucion_actividades.informes i
+    WHERE inf_codigo IN (
+      SELECT iai.inf_codigo
+      FROM ejecucion_actividades.inicio_actividad_informe iai
+      WHERE iai.iac_codigo IN (249)
+    )
+	select * from estructura_poa.actividades a where a.act_codigo in (860)
+	select * from ejecucion_actividades.inicios_actividades ia
+;
+--### DATOS EN BASE
+ 	select *
+    FROM	ejecucion_actividades.inicio_actividad_informe t
+    WHERE	t.iac_codigo in (249)--inf_codigo=17
+   	select *
+    FROM	ejecucion_actividades.inicios_actividades t
+    WHERE	t.iac_codigo in (249)--iac_codigo_control=EBGP120M24F2
+ 	select *
+    FROM	ejecucion_actividades.inicio_actividad_poa t
+    WHERE	t.iac_codigo in (249)--act_codigo=864
+ 	select a.act_descripcion 
+ 	from estructura_poa.actividades a 
+ 	where a.act_codigo in (864)--act_descripcion=GOBIERNO AUTÓNOMO DEPARTAMENTAL DE ORURO
+ 	select * 
+ 	from ejecucion_actividades.informes i 
+ 	where i.inf_codigo in (17)--inf_nombre
+ 	select *
+ 	from parametricas.tipos_trabajos tt 
+ 	where tt.ttr_codigo in (12)
+ 	select ie.eev_codigo  
+ 	from ejecucion_actividades.informes_estados ie  
+ 	where ie.inf_codigo in (17)--no hay nada
+ 	order by ie.fecha_registro desc
+ 	limit 1
+ 	select ee.eev_sigla 
+ 	from parametricas.estados_evolucion ee 
+ 	where ee.eev_codigo in (38)
+ 	SELECT * 
+ 	FROM ejecucion_actividades.informe_recomendaciones ir
+ 	where ir.inf_codigo in (17)
+ 	SELECT COUNT(*) as nrecom 
+ 	FROM ejecucion_actividades.informe_recomendaciones ir
+ 	where ir.ire_estado in (1) AND ir.inf_codigo in (17)
+ 	--#################
+ 	select t.inf_codigo, t.iac_codigo,ia.iac_fecha_inicio, ia.iac_fecha_fin, ia.iac_dias_habiles, ia.iac_dias_calendario, g.ges_anio, ia.iac_codigo_control, tt.ttr_sigla, tt.ttr_descripcion, ia.ttr_codigo , iap.act_codigo, a.act_numero , a.act_descripcion , i2.inf_codigo_control , i2.inf_nombre, ee.eev_sigla
+    FROM	ejecucion_actividades.inicio_actividad_informe t
+    left join ejecucion_actividades.inicios_actividades ia on ia.iac_codigo = t.iac_codigo
+    left join parametricas.tipos_trabajos tt on tt.ttr_codigo = ia.ttr_codigo
+    left join parametricas.gestiones g on g.ges_codigo = ia.ges_codigo
+    left join ejecucion_actividades.inicio_actividad_poa iap on iap.iac_codigo = t.iac_codigo
+    left join estructura_poa.actividades a  on a.act_codigo = iap.act_codigo
+    left join ejecucion_actividades.informes i2 on i2.inf_codigo = t.inf_codigo
+    left join ejecucion_actividades.informes_estados ie on ie.inf_codigo = t.inf_codigo
+    left join parametricas.estados_evolucion ee on ee.eev_codigo = ie.eev_codigo
+    WHERE	t.iac_codigo in (249)--iac_codigo=141
+ 	order by ie.fecha_registro desc
+ 	limit 1
 	;
 --###  --- INICIO ACT POA - AMPLIACION JUSTIFICACION - CAMBIO GLOBAL DE ESTADOS, POR ROL
 	select iap.iap_codigo,iap.iac_codigo,iap.iap_observaciones ,iap.ges_codigo ,iap.iap_estado  from ejecucion_actividades.inicio_actividad_poa iap where iap.iap_codigo in (140)
@@ -141,7 +148,6 @@
 	select * from control_estados.estados_asignaciones_cargos_item eaci  where eaci.aci_codigo in (649) order by eaci.eaci_codigo desc
 	select * from control_estados.estados_asignaciones_cargos_item eaci  where eaci.aci_codigo in (650) order by eaci.eaci_codigo desc
 ;
-
 
 
 
