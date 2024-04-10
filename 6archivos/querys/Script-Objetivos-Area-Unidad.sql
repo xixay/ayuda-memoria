44c1972f697a702daf330fc7428f005351b5286f
Richard Teran <richard_teran@contraloria.gob.bo>
2024-04-10 15:38:06 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-Objetivos-Area-Unidad.sql
+++ 6archivos/querys/Script-Objetivos-Area-Unidad.sql
@@ -36,38 +36,27 @@
 	select * from control_estados.flujos_tablas ft where ft.tab_codigo in (1)
 	select * from estructura_poa.actividades a where a.act_codigo in (1323)
 	select * from control_estados.estados_actividades ea
+	--################################################################################################
 	-- ACTIV
-	select a.act_codigo, a.act_estado, a.pobj_codigo, a.cac_codigo, a.act_justificacion from estructura_poa.actividades a where a.pobj_codigo in (641)
-	-- ACTI ESTAD
-	select * from control_estados.estados_actividades ea where ea.act_codigo in (1362) order by fecha_registro desc
-	select * from estructura_poa.actividades_viaticos av where av.act_codigo in (1359)
-	-- POA OBJE
-	select po.pobj_codigo, po.poa_codigo, po.pobj_estado  from estructura_poa.poas_objetivos po where po.pobj_codigo in (641)
-	-- POA OBJ ESTADO
-	select * from control_estados.estados_poas_objetivos epo where epo.pobj_codigo in (641) order by fecha_registro desc
-	--
-	-- ACTIV
-	select a.act_codigo, a.act_estado, a.pobj_codigo, a.cac_codigo, a.act_justificacion from estructura_poa.actividades a where a.pobj_codigo in (109)
+	select a.act_codigo, a.act_estado, a.pobj_codigo, a.cac_codigo, a.aun_codigo_ejecutora,a.act_justificacion from estructura_poa.actividades a where a.pobj_codigo in (109)
+	select * from estructura_poa.actividades a where a.pobj_codigo in (109)
 	--select * from estructura_poa.actividades a where a.pobj_codigo in (109)
 	-- ACTI ESTAD
 --	select * from control_estados.estados_actividades ea where ea.act_codigo in (1362) order by fecha_registro desc
-	select av.avi_codigo, av.act_codigo ,av.avi_estado from estructura_poa.actividades_viaticos av where av.act_codigo in (19)
+	-- VIATICOS
+	select * from estructura_poa.actividades_viaticos av where av.act_codigo in (19)
 	-- POA OBJE
+	select *  from estructura_poa.poas_objetivos po where po.pobj_codigo in (109)
 	select po.pobj_codigo, po.poa_codigo, po.pobj_estado  from estructura_poa.poas_objetivos po where po.pobj_codigo in (109)
+	select * from estructura_poa.poas_objetivos po where po.poa_codigo in (2)
 	-- POA OBJ ESTADO
-	select * from control_estados.estados_poas_objetivos epo where epo.pobj_codigo in (641) order by fecha_registro desc
+	select * from control_estados.estados_poas_objetivos epo where epo.pobj_codigo in (109) order by fecha_registro desc
+	-- OBJE AREA UNIDAD
+	--select *  from estructura_poa.objetivos_area_unidad oau where oau.pobj_codigo in (109)
 	-- OBJETIVO AREA UNIDAD
 	select oau.oau_codigo,oau.pobj_codigo,oau.oau_estado  from estructura_poa.objetivos_area_unidad oau where oau.pobj_codigo in (641)
-	SELECT	t.tab_nombre, ft.est_codigo_origen, eo.est_nombre, ft.est_codigo_destino, ed.est_nombre
-	FROM	control_estados.flujos_tablas ft
-			LEFT JOIN parametricas.tablas t ON ft.tab_codigo = t.tab_codigo
-			LEFT JOIN parametricas.estados eo ON ft.est_codigo_origen = eo.est_codigo
-			LEFT JOIN parametricas.estados ed ON ft.est_codigo_destino = ed.est_codigo
-	WHERE	ft.tab_codigo IN (SELECT t.tab_codigo FROM parametricas.tablas t WHERE t.tab_nombre IN (
-				--'Poas', 'PoasObjetivos',
-				'Actividades'--, 'ActividadesViaticos'
-			))
-	ORDER BY ft.tab_codigo ASC, ft.est_codigo_origen ASC, ft.est_codigo_destino ASC
+	--POAS
+	select * from estructura_poa.poas p2 where p2.poa_codigo in (2)
 	--
 	SELECT	t.tab_nombre, ft.est_codigo_origen, eo.est_nombre, ft.est_codigo_destino, ed.est_nombre
 	FROM	control_estados.flujos_tablas ft
@@ -76,11 +65,20 @@
 			LEFT JOIN parametricas.estados ed ON ft.est_codigo_destino = ed.est_codigo
 	WHERE	ft.tab_codigo IN (SELECT t.tab_codigo FROM parametricas.tablas t WHERE t.tab_nombre IN (
 				--'Poas', 
-				'PoasObjetivos'--,
-				--'Actividades'--, 
+				--'PoasObjetivos'--,
+				'Actividades'--, 
 				--'ActividadesViaticos'
 			))
-	ORDER BY ft.tab_codigo ASC, ft.est_codigo_origen ASC, ft.est_codigo_destino ASC
+	ORDER BY ft.tab_codigo ASC, ft.est_codigo_origen ASC, ft.est_codigo_destino asc
+	
+	SELECT	po.poa_codigo,p.poa_estado ,po.pobj_codigo , po.pobj_estado, a.act_codigo ,a.cac_codigo,a.act_estado,a.aun_codigo_ejecutora 
+	FROM	estructura_poa.poas p
+			LEFT JOIN estructura_poa.poas_objetivos po on po.poa_codigo = p.poa_codigo
+			LEFT JOIN estructura_poa.actividades a on a.pobj_codigo = po.pobj_codigo
+	WHERE	a.aun_codigo_ejecutora IN (10) and p.poa_codigo  in (2)
+		--WHERE	p.poa_codigo  in (2)
+	--WHERE	a.aun_codigo_ejecutora IN (79) and p.poa_codigo  in (2)
+	ORDER BY po.pobj_codigo DESC 
 ;
 
 
