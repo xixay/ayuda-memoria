c24f2e62c39cd30ec74d2789e790c8279f3d9e1d
Richard Teran <richard_teran@contraloria.gob.bo>
2024-04-18 13:58:40 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-db_update_a.sql
+++ 6archivos/querys/Script-db_update_a.sql
@@ -1,6 +1,28 @@
---ACTIVIDAD
-select	a.act_codigo, 
-		--a.act_numero, 
+        SELECT
+              iac.*,
+              iap.iap_codigo,
+              iap.act_codigo,
+              iap.iap_estado
+        FROM	(
+                SELECT  iac.*, ges.ges_anio
+                FROM	ejecucion_actividades.inicios_actividades iac
+                      LEFT JOIN parametricas.gestiones ges ON iac.ges_codigo = ges.ges_codigo
+                WHERE	TRUE
+                      and iac.iac_estado IN (22)
+              ) iac
+              INNER JOIN
+              (
+                SELECT	*
+                FROM  ejecucion_actividades.inicio_actividad_poa iap
+                WHERE TRUE
+                      AND iap.iap_estado IN (2)
+              ) iap
+              ON iac.iac_codigo = iap.iac_codigo
+        WHERE	TRUE
+        ;
+        
+       select	a.act_codigo, 
+		a.act_numero, 
 		a.tipact_codigo,a.act_objeto,a.ttr_codigo,  --actividad
 		ac.conaud_detalle, ac.conaud_codigo, ac.conaud_correlativo,--al que se le va ser continuidad
 		ia.iac_codigo, ia.iac_codigo_control, ia.iac_codigo_control_vista,--inicio actividad q se crea
@@ -15,15 +37,9 @@ from	estructura_poa.actividades a
 		left join ejecucion_actividades.inicios_actividades ia2 on iap.iac_codigo = ia2.iac_codigo
 		left join estructura_poa.actividades_apoyo aa on a.act_codigo = aa.act_codigo 
 order by a.act_codigo desc
-limit 7
+limit 1
 ;
-select * from estructura_poa.actividades a where a.act_codigo = 1385;
-select * from estructura_poa.actividades_apoyo aa where aa.aap_codigo in (4);
-select * from parametricas.tipos_trabajos tt ;
-select *
-from ejecucion_actividades.inicios_actividades ia
---where ia.iac_codigo = 310
-order by ia.iac_codigo desc
-;
+select * from estructura_poa.actividades a where a.act_codigo = 1366;
+
 
 
