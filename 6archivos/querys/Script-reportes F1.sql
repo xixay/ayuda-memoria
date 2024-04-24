17cc1d657bf8a74ade2b1aa407aa72ca8d22176d
Richard Teran <richard_teran@contraloria.gob.bo>
2024-04-24 18:38:42 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-reportes F1.sql
+++ 6archivos/querys/Script-reportes F1.sql
@@ -1,8 +1,22 @@
-select 	iap.iap_codigo ,iap.iap_estado ,ia.iac_codigo ,ia.iac_estado  
+-- iap_codigo = 227 iap_estado = 17 DEBERIA MOSTRAR 3 FIRMAS
+select 	iap.iap_codigo ,iap.iap_estado ,
+		ia.iac_codigo ,ia.iac_estado  
 from 	ejecucion_actividades.inicio_actividad_poa iap
-left join ejecucion_actividades.inicios_actividades ia on iap.iac_codigo = ia.iac_codigo 
+left join 
+		ejecucion_actividades.inicios_actividades ia on iap.iac_codigo = ia.iac_codigo 
 where 	iap.iap_codigo in (227);
 
-select ia.iac_codigo ,ia.iac_estado 
-from ejecucion_actividades.inicios_actividades ia 
-where ia.iac_codigo in (323);
+select 	ia.iac_codigo ,ia.iac_estado 
+from 	ejecucion_actividades.inicios_actividades ia 
+where 	ia.iac_codigo in (323);
+-- iap_codigo = 1 iap_estado = 2 DEBERIA MOSTRAR 1 FIRMA
+select 	iap.iap_codigo ,iap.iap_estado ,
+		ia.iac_codigo ,ia.iac_estado  
+from 	ejecucion_actividades.inicio_actividad_poa iap
+left join 
+		ejecucion_actividades.inicios_actividades ia on iap.iac_codigo = ia.iac_codigo 
+where 	iap.iap_codigo in (1);
+
+select 	ia.iac_codigo ,ia.iac_estado 
+from 	ejecucion_actividades.inicios_actividades ia 
+where 	ia.iac_codigo in (119);
