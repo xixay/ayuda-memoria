c68e7b904172974e8cc394b5c031a436ae3e7cbd
Richard Teran <richard_teran@contraloria.gob.bo>
2024-04-29 09:54:20 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-reportes F1.sql
+++ 6archivos/querys/Script-reportes F1.sql
@@ -22,10 +22,31 @@ left join
 		ejecucion_actividades.inicios_actividades ia on iap.iac_codigo = ia.iac_codigo 
 where 	iap.iap_codigo in (1);
 
-select 	ia.iac_codigo ,ia.iac_estado 
+select 	*
 from 	ejecucion_actividades.inicios_actividades ia 
-where 	ia.iac_codigo in (119);
+where 	ia.iac_codigo in (285);
+
+--select 	ia.iac_codigo, ia.iac_codigo_control,ia.iac_alcance,ia.ges_codigo 
+select 	ia.iac_codigo, ia.iac_codigo_control, ia.iac_fecha_inicio, ia.ges_codigo 
+from 	ejecucion_actividades.inicios_actividades ia 
+--where 	ia.iac_codigo_control = 'GHEP25A97'
+--where 	ia.iac_migrado = true
+order by ia.iac_codigo desc;
+--AÑO
+select 	*
+from 	parametricas.gestiones g
+where 	g.ges_anio  in (2023);
 --ACTIVIDADES act_codigo = 284
 select 	*
 from	estructura_poa.actividades a
 where 	a.aun_codigo_ejecutora in (59);
+--TIPOS DE TRABAJO
+select	*
+from 	parametricas.tipos_trabajos tt ;
+---
+SELECT iac.*, ges.ges_anio
+FROM ejecucion_actividades.inicios_actividades iac
+LEFT JOIN parametricas.gestiones ges ON iac.ges_codigo = ges.ges_codigo
+WHERE iac.iac_estado in (22)
+AND iac.iac_codigo_control IN ('ELEN36A99');
+
