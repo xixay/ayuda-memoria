8736181ef76a41ba8822f80fe06c78a3ef3a143d
rteran <richard_teran@contraloria.gob.bo>
2024-07-02 00:16:33 -0400

    Actualizar scripts de DBeaver

--- 6archivos/scripts/backup_base_remota.sh
+++ 6archivos/scripts/backup_base_remota.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-remote_host="172.16.22.234"
-remote_port="5432"
-remote_db="db_poa_update_a"
-remote_user="postgres"
-remote_password="postgres"
-local_db="bd_cge_poa_conaud_local_b"
-local_user="postgres"
-local_password="postgres"
+
+# Función para mostrar un cuadro de entrada Zenity y obtener el valor ingresado
+input_box() {
+    local title="$1"
+    local prompt="$2"
+    local default_value="$3"
+    zenity --entry --title="$title" --text="$prompt" --entry-text="$default_value" 2>/dev/null
+}
+
+# Obtener los datos de conexión usando Zenity
+remote_host=$(input_box "Datos de conexión remota" "Ingrese el host remoto" "172.16.22.234")
+remote_port=$(input_box "Datos de conexión remota" "Ingrese el puerto remoto" "5432")
+remote_db=$(input_box "Datos de conexión remota" "Ingrese la base de datos remota" "db_poa_update_a")
+remote_user=$(input_box "Datos de conexión remota" "Ingrese el usuario remoto" "postgres")
+remote_password=$(input_box "Datos de conexión remota" "Ingrese la contraseña remota" "postgres")
+
+local_db=$(input_box "Datos de conexión local" "Ingrese la base de datos local" "bd_cge_poa_conaud_local_b")
+local_user=$(input_box "Datos de conexión local" "Ingrese el usuario local" "postgres")
+local_password=$(input_box "Datos de conexión local" "Ingrese la contraseña local" "postgres")
+
 backup_file="backup_$(date '+%Y%m%d_%H%M%S').sql"
 
 # Especificar la ruta completa de pg_dump y pg_restore para PostgreSQL 16
@@ -15,7 +27,7 @@ pg_restore_path="/usr/bin/pg_restore"  # Asegúrate de que esta es la ruta corre
 
 # Verificar si pg_dump existe en la ruta especificada
 if [ ! -f "$pg_dump_path" ]; then
-    echo "Error: $pg_dump_path no existe."
+    zenity --error --text="Error: $pg_dump_path no existe."
     exit 1
 fi
 
@@ -24,7 +36,7 @@ PGPASSWORD=$remote_password $pg_dump_path -h $remote_host -p $remote_port -U $re
 
 # Verificar si el archivo de respaldo se creó correctamente
 if [ ! -f "$backup_file" ]; then
-    echo "Error: No se pudo crear el archivo de respaldo $backup_file."
+    zenity --error --text="Error: No se pudo crear el archivo de respaldo $backup_file."
     exit 1
 fi
 
@@ -35,7 +47,9 @@ PGPASSWORD=$local_password $pg_restore_path -h localhost -p 5432 -U $local_user 
 # rm $backup_file
 
 # Notificar la finalización del proceso
-notify-send "Copia de seguridad y restauración completadas" "El proceso de copia de seguridad y restauración se completó correctamente."
+zenity --info --text="Copia de seguridad y restauración completadas.\nEl proceso se ha completado correctamente."
+
+exit 0
 
 ##################################INstalacion de postgres 16########################################
 # Agregar el repositorio oficial de PostgreSQL para la versión 16
