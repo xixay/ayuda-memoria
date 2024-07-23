255debca735940953304eb50cb288f44cd756c72
Richard Teran <richard_teran@contraloria.gob.bo>
2024-07-23 09:52:10 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-retiro-aprob-GI.sql
+++ 6archivos/querys/Script-retiro-aprob-GI.sql
@@ -7,10 +7,6 @@ WHERE 	a.act_numero = '00.0001.3.1.24'
 --
 SELECT 	*
 FROM 	parametricas.estados e ;
---ESTADO RETIRO
-INSERT INTO parametricas.estados
-(est_codigo, est_nombre, est_color, est_descripcion, usuario_registro, usuario_modificacion, usuario_baja, fecha_registro, fecha_modificacion, fecha_baja)
-VALUES(44, 'RETIRO APROBADO GI.', '#ea5545', 'Retiro Aprobado por Gestión Institucional.', 0, 0, 0, '2024-05-09 18:53:52.462', '1900-01-01 00:00:00.000', '1900-01-01 00:00:00.000');
 --FLUJO
 SELECT	t.tab_nombre, ft.est_codigo_origen, eo.est_nombre, ft.est_codigo_destino, ed.est_nombre
 FROM	control_estados.flujos_tablas ft
@@ -25,6 +21,10 @@ SELECT 	*
 FROM 	control_estados.flujos_tablas ft
 ORDER BY ft.fta_codigo ASC
 ;
+--ESTADO RETIRO
+INSERT INTO parametricas.estados
+(est_codigo, est_nombre, est_color, est_descripcion, usuario_registro, usuario_modificacion, usuario_baja, fecha_registro, fecha_modificacion, fecha_baja)
+VALUES(44, 'RETIRO APROBADO GI.', '#ea5545', 'Retiro Aprobado por Gestión Institucional.', 0, 0, 0, '2024-05-09 18:53:52.462', '1900-01-01 00:00:00.000', '1900-01-01 00:00:00.000');
 --ACTIVIDAD AGREGAR EL NUEVO FLUJO(13-44)
 INSERT INTO control_estados.flujos_tablas
 (fta_codigo, tab_codigo, fta_descripcion, est_codigo_origen, est_codigo_destino, fta_estado, usuario_registro, usuario_modificacion, usuario_baja, fecha_registro, fecha_modificacion, fecha_baja)
@@ -33,7 +33,7 @@ VALUES(371, 1, '', 13, 44, 1, 0, 0, 0, '2024-05-09 18:57:05.209', '1900-01-01 00
 INSERT INTO control_estados.flujos_tablas
 (fta_codigo, tab_codigo, fta_descripcion, est_codigo_origen, est_codigo_destino, fta_estado, usuario_registro, usuario_modificacion, usuario_baja, fecha_registro, fecha_modificacion, fecha_baja)
 VALUES(372, 1, '', 44, 14, 1, 0, 0, 0, '2024-05-09 18:57:05.209', '1900-01-01 00:00:00.000', '1900-01-01 00:00:00.000');
---ACTIVIDAD AGREGAR EL NUEVO FLUJO(44-14)
+--ACTIVIDAD AGREGAR EL NUEVO FLUJO(44-9)
 INSERT INTO control_estados.flujos_tablas
 (fta_codigo, tab_codigo, fta_descripcion, est_codigo_origen, est_codigo_destino, fta_estado, usuario_registro, usuario_modificacion, usuario_baja, fecha_registro, fecha_modificacion, fecha_baja)
 VALUES(373, 1, '', 44, 9, 1, 0, 0, 0, '2024-05-09 18:57:05.209', '1900-01-01 00:00:00.000', '1900-01-01 00:00:00.000');
