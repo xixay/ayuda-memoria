e4a922feae34ebd27fbb6292e3c94a6e69dc05ae
Richard Teran <richard_teran@contraloria.gob.bo>
2024-08-05 14:44:21 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-8.sql
+++ 6archivos/querys/Script-8.sql
@@ -1,4 +1,5 @@
 SELECT 	*
 FROM	estructura_poa.actividades a 
-WHERE 	a.act_estado IN (9)
+--WHERE 	a.act_estado IN (9)
+WHERE 	a.act_codigo_anterior IN (880,94,991,1004,883,770,774,783,260,768,988,987,118)
 ;
