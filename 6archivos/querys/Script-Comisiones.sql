bb25d0055cb62afaf26c2e2466ffa3c08d10093b
Richard Teran <richard_teran@contraloria.gob.bo>
2024-08-08 14:37:05 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-Comisiones.sql
+++ 6archivos/querys/Script-Comisiones.sql
@@ -2,11 +2,12 @@ SELECT	aci.*
 FROM	ejecucion_poa.asignaciones_cargos_item aci
 		LEFT JOIN estructura_organizacional.cargos_items_persona cip ON aci.cit_codigo = cip.cit_codigo
 WHERE	TRUE
-		AND cip.cip_estado NOT IN (5)
+--		AND cip.cip_estado NOT IN (5)
 --		AND aci.asi_codigo = 1384--ELIMINO CLAUDIA VERONICA AILLON CRESPO aci_codigo=5121
 --		AND aci.asi_codigo = 1387--520.1302.61.9.24 ELIMINO MARCELO MATEO GARCIA ESCOBAR aci_codigo=5131, LIDIA MENECES GABRIEL aci_codigo=5130, JOSUE ROYO SORIA aci_codigo=5132 
-		AND aci.asi_codigo = 162
+--		AND aci.asi_codigo = 162
 --		AND aci.asi_codigo = 1385
+		AND aci.asi_codigo = 1358
 --		AND cip.per_codigo IN (216)
 --		AND aci.cit_codigo IN (564)
 --		AND aci.persona_detalle NOTNULL 
