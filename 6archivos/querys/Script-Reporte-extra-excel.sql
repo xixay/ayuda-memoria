e43f4fa0a547fdfc86b4649b86e4bb71d1029524
Richard Teran <richard_teran@contraloria.gob.bo>
2024-08-12 22:21:29 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-Reporte-extra-excel.sql
+++ 6archivos/querys/Script-Reporte-extra-excel.sql
@@ -10,3 +10,10 @@ FROM 	  estructura_poa.poas_objetivos po
 WHERE   TRUE
         AND po.pobj_codigo IN (777,1180,1181,1182,1183,1184,1185,1186,1187,1188,1189,1190,1191,1192,1193,1194,1195,1196,1197,1198,1199,1200,1201,1202,1203,1204,1205,1206,1207,1208,1209,1210)
         ;
+        
+SELECT 	*
+FROM 	estructura_organizacional.areas_unidades au
+WHERE 	TRUE 
+		AND au.aun_codigo IN (17)--padre
+;
+
