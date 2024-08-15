91e42b79b678133c363e133a37a341df08921066
Richard Teran <richard_teran@contraloria.gob.bo>
2024-08-15 15:27:18 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-CONAUD.sql
+++ 6archivos/querys/Script-CONAUD.sql
@@ -1,14 +1,17 @@
-SELECT DISTINCT 
-    ia.iac_codigo,
-    ia.iac_codigo_control AS trabajo_codigo,
-    ac.iac_codigo, 
-    ac.conaud_correlativo,
-    ac.conaud_detalle::text  -- Convertir JSON a texto
-FROM 
-    ejecucion_actividades.inicios_actividades ia
-    LEFT JOIN estructura_poa.actividades_continuidad ac ON ia.iac_codigo = ac.iac_codigo
-WHERE 
-    ia.iac_codigo_control LIKE '%Y22';
+SELECT 	DISTINCT 
+		ia.iac_codigo,
+    	ia.iac_codigo_control AS trabajo_codigo,
+    	ac.conaud_detalle->>'entidad_descripcion' AS entidad_descripcion,
+    	ia.iac_objeto AS auditoria_objeto,
+    	ia.iac_objetivo AS auditoria_objetivo,
+    	g.ges_anio AS actividad_gestion,
+    	ac.conaud_detalle->>'correlativo_codigo' AS correlativo_codigo
+FROM 	ejecucion_actividades.inicios_actividades ia
+		LEFT JOIN estructura_poa.actividades_continuidad ac ON ia.iac_codigo = ac.iac_codigo
+		LEFT JOIN parametricas.gestiones g ON ia.ges_codigo = g.ges_codigo 
+WHERE 	ia.iac_codigo_control LIKE '%J23';
+--WHERE 	ia.iac_codigo_control LIKE 'SPGP32Y22';
+
    
 SELECT DISTINCT 
     ia.iac_codigo,
@@ -28,7 +31,8 @@ FROM
     LEFT JOIN estructura_poa.actividades_continuidad ac ON ia.iac_codigo = ac.iac_codigo
 WHERE
 	ia.iac_estado NOT IN (0)
-    AND ia.iac_codigo_control LIKE '%Y22';
+--    AND ia.iac_codigo_control LIKE '%Y22';
+    AND ia.iac_codigo_control LIKE 'GPEP66Y22';
 
 
 
@@ -40,6 +44,9 @@ FROM 	ejecucion_actividades.inicios_actividades ia
 SELECT 	*
 FROM 	estructura_poa.actividades_continuidad ac ;
 
+SELECT 	*
+FROM 	estructura_poa.actividades a
+ORDER	BY a.act_codigo DESC ;
 
 
 
