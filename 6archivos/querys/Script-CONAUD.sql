5ee36b8114ad8dd21883e63326b8652ff4d68ad5
Richard Teran <richard_teran@contraloria.gob.bo>
2024-08-15 20:01:09 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-CONAUD.sql
+++ 6archivos/querys/Script-CONAUD.sql
@@ -1,57 +1,51 @@
 SELECT 	DISTINCT 
 		ia.iac_codigo,
     	ia.iac_codigo_control AS trabajo_codigo,
-    	ac.conaud_detalle->>'entidad_descripcion' AS entidad_descripcion,
     	ia.iac_objeto AS auditoria_objeto,
     	ia.iac_objetivo AS auditoria_objetivo,
-    	g.ges_anio AS actividad_gestion,
-    	ac.conaud_detalle->>'correlativo_codigo' AS correlativo_codigo
+    	g.ges_anio AS actividad_gestion
 FROM 	ejecucion_actividades.inicios_actividades ia
 		LEFT JOIN estructura_poa.actividades_continuidad ac ON ia.iac_codigo = ac.iac_codigo
 		LEFT JOIN parametricas.gestiones g ON ia.ges_codigo = g.ges_codigo 
-WHERE 	ia.iac_codigo_control LIKE '%J23';
---WHERE 	ia.iac_codigo_control LIKE 'SPGP32Y22';
+WHERE 	ia.iac_estado NOT IN (0)
+		AND ia.iac_codigo_control LIKE '%Y22';
+--		AND ia.iac_codigo_control LIKE '%J23';
 
-   
-SELECT DISTINCT 
-    ia.iac_codigo,
-    ia.iac_estado,
-    -- Extraer correlativo_codigo del JSON si existe, de lo contrario usar el valor por defecto
-    COALESCE(ac.conaud_detalle->>'correlativo_codigo', ia.iac_correlativo::text) AS correlativo_codigo, 
-    -- Extraer trabajo_codigo del JSON si existe, de lo contrario usar el valor por defecto
-    COALESCE(ac.conaud_detalle->>'trabajo_codigo', ia.iac_codigo_control) AS trabajo_codigo,
-    -- Extraer actividad_codigo del JSON si existe, o NULL si no existe
-    ac.conaud_detalle->>'actividad_codigo' AS actividad_codigo,
-    -- Extraer entidad_descripcion del JSON si existe, de lo contrario usar un valor por defecto o NULL
-    ac.conaud_detalle->>'entidad_descripcion' AS entidad_descripcion,
-    ac.iac_codigo, 
-    ac.conaud_correlativo
-FROM 
-    ejecucion_actividades.inicios_actividades ia
-    LEFT JOIN estructura_poa.actividades_continuidad ac ON ia.iac_codigo = ac.iac_codigo
-WHERE
-	ia.iac_estado NOT IN (0)
---    AND ia.iac_codigo_control LIKE '%Y22';
-    AND ia.iac_codigo_control LIKE 'GPEP66Y22';
+SELECT 	ac.aco_codigo , ac.aco_estado , ac.conaud_codigo , ac.conaud_correlativo, ac.conaud_detalle, ac.act_codigo , ac.iac_codigo ,
+		ia.iac_codigo_control 
+FROM 	estructura_poa.actividades_continuidad ac
+		LEFT JOIN ejecucion_actividades.inicios_actividades ia ON ac.iac_codigo = ia.iac_codigo
+WHERE 	TRUE 
+		AND ia.iac_estado NOT IN (0)
+--		AND ia.iac_codigo_control LIKE '%Y22'
+		AND ia.iac_codigo_control LIKE '%J23'
+ORDER BY ac.aco_codigo DESC 
+;
+ 
+SELECT 	ia.iac_codigo , ia.iac_correlativo , ia.iac_codigo_control 
+FROM 	ejecucion_actividades.inicios_actividades ia
+WHERE 	ia.iac_codigo_control LIKE '%Y22'
+ORDER BY ia.iac_codigo DESC
+LIMIT 1
+;
 
 
-
-
+SELECT	DISTINCT 
+		ia.iac_codigo,
+    	ac.conaud_detalle->>'entidad_descripcion' AS entidad_descripcion
+FROM 	ejecucion_actividades.inicios_actividades ia
+		LEFT JOIN estructura_poa.actividades_continuidad ac ON ia.iac_codigo = ac.iac_codigo
+WHERE 	ia.iac_estado NOT IN (0)
+		AND ia.iac_codigo_control LIKE 'GCEP69Y22';
+	
 SELECT 	*
-FROM 	ejecucion_actividades.inicios_actividades ia 
+FROM 	estructura_poa.actividades_continuidad ac
+ORDER BY ac.aco_codigo DESC
 ;
 
 SELECT 	*
-FROM 	estructura_poa.actividades_continuidad ac ;
-
-SELECT 	*
-FROM 	estructura_poa.actividades a
-ORDER	BY a.act_codigo DESC ;
-
-
-
-
-
+FROM 	estructura_poa.actividades a 
+ORDER BY a.act_codigo DESC ;
 
 
 
