3dab803d3dbec42bbe3369faeb93483c2abc79eb
Richard Teran <richard_teran@contraloria.gob.bo>
2024-08-16 12:19:29 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-CONAUD.sql
+++ 6archivos/querys/Script-CONAUD.sql
@@ -1,6 +1,7 @@
 SELECT 	DISTINCT 
 		ia.iac_codigo,
     	ia.iac_codigo_control AS trabajo_codigo,
+    	ac.conaud_detalle->>'entidad_descripcion' AS entidad_descripcion,
     	ia.iac_objeto AS auditoria_objeto,
     	ia.iac_objetivo AS auditoria_objetivo,
     	g.ges_anio AS actividad_gestion
@@ -8,7 +9,8 @@ FROM 	ejecucion_actividades.inicios_actividades ia
 		LEFT JOIN estructura_poa.actividades_continuidad ac ON ia.iac_codigo = ac.iac_codigo
 		LEFT JOIN parametricas.gestiones g ON ia.ges_codigo = g.ges_codigo 
 WHERE 	ia.iac_estado NOT IN (0)
-		AND ia.iac_codigo_control LIKE '%Y22';
+--		AND ia.iac_codigo_control LIKE '%Y24';
+		AND ia.iac_codigo_control LIKE '%Y22';--GCEP69Y22
 --		AND ia.iac_codigo_control LIKE '%J23';
 
 SELECT 	ac.aco_codigo , ac.aco_estado , ac.conaud_codigo , ac.conaud_correlativo, ac.conaud_detalle, ac.act_codigo , ac.iac_codigo ,
@@ -17,16 +19,10 @@ FROM 	estructura_poa.actividades_continuidad ac
 		LEFT JOIN ejecucion_actividades.inicios_actividades ia ON ac.iac_codigo = ia.iac_codigo
 WHERE 	TRUE 
 		AND ia.iac_estado NOT IN (0)
+		AND ac.iac_codigo IN (117)
 --		AND ia.iac_codigo_control LIKE '%Y22'
-		AND ia.iac_codigo_control LIKE '%J23'
+--		AND ia.iac_codigo_control LIKE '%J23'
 ORDER BY ac.aco_codigo DESC 
-;
- 
-SELECT 	ia.iac_codigo , ia.iac_correlativo , ia.iac_codigo_control 
-FROM 	ejecucion_actividades.inicios_actividades ia
-WHERE 	ia.iac_codigo_control LIKE '%Y22'
-ORDER BY ia.iac_codigo DESC
-LIMIT 1
 ;
 
 
@@ -38,9 +34,16 @@ FROM 	ejecucion_actividades.inicios_actividades ia
 WHERE 	ia.iac_estado NOT IN (0)
 		AND ia.iac_codigo_control LIKE 'GCEP69Y22';
 	
-SELECT 	*
+SELECT 	*--ac.aco_codigo , ac.conaud_codigo , ac.conaud_correlativo, ac.conaud_detalle,ac.iac_codigo 
 FROM 	estructura_poa.actividades_continuidad ac
+--WHERE 	ac.aco_codigo IN (320)
 ORDER BY ac.aco_codigo DESC
+;
+
+SELECT 	*--ia.iac_codigo , ia.iac_correlativo , ia.iac_codigo_control 
+FROM 	ejecucion_actividades.inicios_actividades ia
+WHERE 	ia.iac_codigo_control LIKE 'P5FP41Y22'
+ORDER BY ia.iac_codigo DESC
 ;
 
 SELECT 	*
