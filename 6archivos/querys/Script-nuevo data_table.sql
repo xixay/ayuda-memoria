cb47498bf7278131e80d2c7081059d0e7ee6b17b
Richard Teran <richard_teran@contraloria.gob.bo>
2024-08-20 11:21:19 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-nuevo data_table.sql
+++ 6archivos/querys/Script-nuevo data_table.sql
@@ -1,9 +1,11 @@
 SELECT 	*
 FROM 	estructura_poa.poas_objetivos po 
+WHERE 	po.pobj_codigo = 1662
 ORDER BY po.pobj_codigo DESC 
 ;
 
 SELECT 	*
-FROM 	estructura_poa.objetivos_area_unidad oau 
+FROM 	estructura_poa.objetivos_area_unidad oau
+WHERE 	oau.pobj_codigo = 1662
 ORDER BY oau.pobj_codigo DESC 
 ;
