38d42bc081b5abf9533c6950a956eb41cb95b499
Richard Teran <richard_teran@contraloria.gob.bo>
2024-09-13 18:33:46 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-Nuevo.sql
+++ 6archivos/querys/Script-Nuevo.sql
@@ -1,4 +1,22 @@
 SELECT 	*
-FROM 	estructura_poa.actividades a 
-WHERE 	a.act_codigo IN (2982)
+FROM 	estructura_poa.actividades a
+--WHERE 	a.act_codigo IN (2982)
+ORDER BY a.act_codigo DESC
 ;
+
+SELECT 	*
+FROM 	estructura_poa.actividades_continuidad ac 
+ORDER BY ac.aco_codigo DESC
+;
+
+SELECT 	*
+FROM 	ejecucion_actividades.inicios_actividades ia
+ORDER BY ia.iac_codigo DESC 
+;
+
+SELECT 	*
+FROM 	ejecucion_actividades.inicio_actividad_poa iap 
+ORDER BY iap.iap_codigo DESC
+;
+SELECT 	*
+FROM 	parametricas.estados e ;
