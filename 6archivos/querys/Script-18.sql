4833991fe82c5f943fe5a0e87d90e2f359bf3126
Richard Teran <richard_teran@contraloria.gob.bo>
2024-09-27 16:53:09 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-18.sql
+++ 6archivos/querys/Script-18.sql
@@ -1,7 +1,12 @@
 SELECT 	*
-FROM 	parametricas.empresas e
+FROM 	estructura_organizacional.cargos_items_persona cip
+WHERE 	cip.cit_codigo IN (494,522,572,94)
 ;
+
 SELECT 	*
 FROM 	ejecucion_poa.asignaciones_cargos_item aci
+WHERE 	TRUE
+		AND aci.asi_codigo IN (482)
+		AND aci.aci_estado IN (1,2,21,9)
 ORDER BY aci.aci_codigo DESC
 ;
