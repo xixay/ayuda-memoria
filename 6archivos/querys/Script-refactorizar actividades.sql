cb035e59796bf497c79bd4b326041d600d7b4914
Richard Teran <richard_teran@contraloria.gob.bo>
2024-09-30 21:29:33 -0400

    Actualizar scripts de DBeaver

---
+++ 6archivos/querys/Script-refactorizar actividades.sql
@@ -0,0 +1,28 @@
+--###################### ROLES #######################
+--FORMULARIOS TODOS
+          SELECT
+                au.aun_codigo AS aun_codigo_ejecutora, au.aun_nombre, au.aun_sigla, au.cau_codigo, au.aun_numero,
+                CONCAT(au.aun_nombre, ' - ', au.aun_sigla) AS nom_ejecutora,
+                au.aun_reporte_habilitado,
+                COALESCE(
+                  ARRAY_AGG(
+                    DISTINCT aur.rol_codigo ORDER BY aur.rol_codigo ASC
+                  ) FILTER (WHERE aur.per_codigo = 1914),
+                  '{}'
+                ) AS roles,
+                COUNT(DISTINCT aur.rol_codigo) AS cantidad_roles
+          FROM  estructura_poa.area_unidad_responsables aur
+                LEFT JOIN estructura_organizacional.areas_unidades au ON aur.aun_codigo_ejecutora = au.aun_codigo
+          WHERE aur.aur_estado != 0 -- ESTADO ROL-RESPONSABLE
+                AND au.aun_estado IN (2,8) -- ESTADO AREA-UNIDAD (CONSOLIDADO, APROBADO)
+                AND aur.rol_codigo IN (1,2,3,4,8) -- ROL SELECCIONADO
+                AND aur.poa_codigo IN (3) -- POA SELECCIONADO
+                 -- PER_CODIGO SELECCIONADO
+          GROUP BY au.aun_codigo, au.aun_nombre, au.aun_sigla, au.cau_codigo, au.aun_numero, au.aun_reporte_habilitado
+          ORDER BY au.aun_sigla ASC, au.aun_nombre ASC
+          ;
+--SOLO UNO
+SELECT 	*
+FROM 	parametricas.estados e
+;
+
