af509a5b5db14fb213fc7b1ff0363feb5eb07697
Richard Teran <richard_teran@contraloria.gob.bo>
2024-12-12 09:17:07 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-17.sql
+++ 6archivos/querys/Script-17.sql
@@ -1,71 +1,120 @@
 SELECT 	*
-FROM 	estructura_poa.poas_objetivos po 
-WHERE 	TRUE
-		AND po.poa_codigo IN (3)
-		AND po.pobj_nombre LIKE 'Emitir 6 informes analíticos de los Planes Anuales de Auditoría elaborados por las UAI de Entidades Públicas del Nivel Departamental en la Gerencia Departamental del Beni en la gestión 2024.'
+FROM 	estructura_poa.actividades a
+ORDER BY a.act_codigo DESC
 ;
 
+SELECT	*
+FROM 	estructura_poa.actividades_continuidad ac 
+WHERE 	TRUE
+--		AND ac.aco_codigo IN (398)
+ORDER BY ac.aco_codigo DESC
+;
 
 SELECT 	*
-FROM 	estructura_poa.area_unidad_responsables aur 
+FROM 	ejecucion_actividades.inicios_actividades ia
 WHERE 	TRUE
-		AND aur.per_codigo IN (79)
+--		AND ia.iac_codigo IN (309)
+--		AND ia.iac_alcance IS NULL
+--		AND ia.iac_codigo_control LIKE 'K4GP'
+ORDER BY ia.iac_codigo DESC
 ;
+
+SELECT 	*
+FROM 	ejecucion_actividades.actividad_migrada_conaud amc
+ORDER BY amc.amc_codigo DESC
+;
+
+--############################################
+        SELECT
+              t.iac_codigo,
+              t.iac_correlativo,
+              t.iac_codigo_control,
+              t.iac_codigo_control_vista,
+              t.iac_objeto,
+              t.iac_objetivo,
+              COALESCE(t.iac_alcance, '') AS iac_alcance,
+              t.iac_migrado,
+              t.iac_observaciones,
+              TO_CHAR(t.iac_fecha_inicio, 'dd/mm/yyyy') AS iac_fecha_inicio,
+              t.ttr_codigo, tt.ttr_sigla, tt.ttr_descripcion,
+              t.iac_dias_habiles,
+              t.iac_dias_calendario,
+              TO_CHAR(t.iac_fecha_fin, 'dd/mm/yyyy') AS iac_fecha_fin,
+              t.ttr_codigo,
+              TO_CHAR(t.iac_mes_inicio, 'dd/mm/yyyy') AS iac_mes_inicio,
+              TO_CHAR(t.iac_mes_fin, 'dd/mm/yyyy') AS iac_mes_fin,
+              TO_CHAR(t.iac_mes_inicio, 'mm/yyyy') AS mes_inicio,
+              TO_CHAR(t.iac_mes_fin, 'mm/yyyy') AS mes_fin,
+              TO_CHAR(t.iac_fecha_borrador, 'dd/mm/yyyy') AS iac_fecha_borrador,
+              TO_CHAR(t.iac_fecha_emision, 'dd/mm/yyyy') AS iac_fecha_emision,
+              t.ges_codigo,
+              g.ges_anio,
+              t.iac_estado,
+              e.est_color,
+              e.est_nombre AS iac_estado_descripcion,
+              TO_CHAR(t.iac_fecha_inicio_historico, 'dd/mm/yyyy') AS iac_fecha_inicio_historico,
+              TO_CHAR(t.fecha_registro, 'HH24:MI am dd/mm/yyyy') AS fecha_registro,
+              COALESCE(iaa.iaa_codigo, 0) iaa_codigo,
+              t.iac_recomendacion_seguir,
+              t.usuario_registro,
+              t.usuario_modificacion,
+              TO_CHAR(t.fecha_modificacion, 'HH24:MI am dd/mm/yyyy') AS fecha_modificacion
+        FROM    ejecucion_actividades.inicios_actividades t
+              LEFT JOIN ejecucion_actividades.inicios_actividades_adicional iaa ON t.iac_codigo = iaa.iac_codigo
+              LEFT JOIN parametricas.estados e ON t.iac_estado = e.est_codigo
+              LEFT JOIN parametricas.gestiones g ON t.ges_codigo = g.ges_codigo
+              LEFT JOIN parametricas.tipos_trabajos tt ON t.ttr_codigo = tt.ttr_codigo
+        WHERE   TRUE
+              AND t.iac_codigo IN (701)
+        ORDER BY t.fecha_registro DESC
+        ;
+
+       
+SELECT 	*
+FROM 	estructura_organizacional.cargos_items_persona cip
+ORDER BY cip.cip_codigo DESC
+;
+
 --
+          WITH tmp_cargo_item_persona as ( -- Filtro de cargos items persona
+            SELECT cip.cip_codigo, cip.cip_estado, cip.cit_codigo, cip.per_codigo
+            FROM estructura_organizacional.cargos_items_persona cip
+            --WHERE cip_estado NOT IN (0,5)
+            WHERE cip_estado NOT IN (0)
+          )
           SELECT
-                au.aun_codigo AS aun_codigo_ejecutora, au.aun_nombre, au.aun_sigla--,
-                --CONCAT(au.aun_nombre, ' - ', au.aun_sigla) AS nom_ejecutora
-          FROM  estructura_poa.area_unidad_responsables aur
-                LEFT JOIN estructura_organizacional.areas_unidades au ON aur.aun_codigo_ejecutora = au.aun_codigo
-          WHERE aur.aur_estado != 0 -- ESTADO ROL-RESPONSABLE
-                AND au.aun_estado IN (2,8) -- ESTADO AREA-UNIDAD (CONSOLIDADO, APROBADO)
-                AND aur.rol_codigo IN (1,2,3,4,8) -- ROL SELECCIONADO
-                AND aur.poa_codigo IN (3) -- POA SELECCIONADO
-                AND aur.per_codigo = 79 -- PER_CODIGO SELECCIONADO
-               -- AND au.aun_codigo IN (79,46)
-          GROUP BY au.aun_codigo, au.aun_nombre, au.aun_sigla, au.cau_codigo, au.aun_numero, au.aun_reporte_habilitado
-          ORDER BY au.aun_sigla ASC, au.aun_nombre ASC
-          ;
---$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-SELECT 	au.aun_codigo ,au.aun_sigla, au.aun_estado 
-FROM 	estructura_organizacional.areas_unidades au 
-WHERE 	TRUE
-		AND au.aun_codigo IN (4,13,17,21,26,31,36,41,44,47)--padre
-		--AND au.aun_codigo IN (4,13,63,18,23,28,31,33,38,40,43,46,79,49)--hijos
-;
---%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-WITH agrupado AS (
-    SELECT
-        -- Extraemos la parte de la sigla antes del guion (o toda la cadena si no hay guion)
-        COALESCE(NULLIF(SPLIT_PART(au.aun_sigla, '-', 1), ''), au.aun_sigla) AS sigla_base,
-        STRING_AGG(DISTINCT au.aun_codigo::TEXT, ',') AS aun_codigo_agrupado -- Eliminamos duplicados
-    FROM estructura_poa.area_unidad_responsables aur
-         LEFT JOIN estructura_organizacional.areas_unidades au 
-         ON aur.aun_codigo_ejecutora = au.aun_codigo
-    WHERE aur.aur_estado != 0 -- ESTADO ROL-RESPONSABLE
-          AND au.aun_estado IN (2, 8) -- ESTADO AREA-UNIDAD (CONSOLIDADO, APROBADO)
-          AND aur.rol_codigo IN (1, 2, 3, 4, 8) -- ROL SELECCIONADO
-          AND aur.poa_codigo IN (3) -- POA SELECCIONADO
-          AND aur.per_codigo = 79 -- PER_CODIGO SELECCIONADO
-    GROUP BY COALESCE(NULLIF(SPLIT_PART(au.aun_sigla, '-', 1), ''), au.aun_sigla) -- Agrupamos por la unidad principal
-)
-SELECT 
-    sigla_base AS aun_sigla,
-    '(' || aun_codigo_agrupado || ')' AS aun_codigo
-FROM agrupado
-ORDER BY sigla_base;
---##################################################################################################3
-SELECT 	
-		aud.aun_codigo_padre, aud.aun_codigo_hijo ,
-		au.aun_codigo, CONCAT('(', au.aun_numero, ') ', au.aun_nombre, ' - ', au.aun_sigla) AS aun_concatenado_invert
-FROM 	estructura_organizacional.areas_unidades_dependencias aud
-		LEFT JOIN estructura_organizacional.areas_unidades au ON aud.aun_codigo_padre = au.aun_codigo 
-WHERE 	TRUE
-		AND aud.aud_estado IN (1)
-		AND aud.aun_codigo_padre IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13, 17, 21, 26, 31, 36, 41, 44, 47, 57,58, 68, 81)
-;
-
-
-
+                  t.cit_codigo,
+                  t.cit_descripcion,
+                  t.car_codigo,
+                  t.ite_codigo,
+                  t.aun_codigo,
+                  t.cit_estado,
+                  au.aun_nombre,
+                  au.aun_sigla,
+                  CONCAT_WS(' - ', au.aun_sigla, au.aun_nombre) AS aun_concatenado,
+                  CONCAT_WS(' - ', au.aun_nombre, au.aun_sigla) AS aun_concatenado_invert,
+                  au.aun_numero,
+                  ( CASE WHEN (t.cit_estado = 42) THEN CONCAT( c.car_nombre, ' (', e.est_nombre,')' ) ELSE c.car_nombre END ) AS car_nombre,
+                  c.car_alias,
+                  tc.tca_nombre,
+                  i.ite_descripcion,
+                  i.ite_numero,
+                  CONCAT_WS(' - ', i.ite_numero, c.car_nombre) car_nombre_item,
+                  cip.cip_codigo,
+                  cip.cip_estado,
+                  cip.per_codigo,
+                  e.est_color,
+                  e.est_nombre AS cit_estado_descripcion
+          FROM    estructura_organizacional.cargos_items t
+                  LEFT JOIN parametricas.estados e ON e.est_codigo = t.cit_estado
+                  LEFT JOIN estructura_organizacional.areas_unidades au ON au.aun_codigo = t.aun_codigo
+                  LEFT JOIN estructura_organizacional.cargos c ON c.car_codigo = t.car_codigo
+                  LEFT JOIN parametricas.tipos_cargos tc ON tc.tca_codigo = c.tca_codigo
+                  LEFT JOIN estructura_organizacional.items i  ON i.ite_codigo = t.ite_codigo
+                  LEFT JOIN tmp_cargo_item_persona cip  ON cip.cit_codigo = t.cit_codigo
+          WHERE   TRUE
+		          AND cip.cip_estado IN (2)
+                  AND t.cit_codigo IN (134,125,144,152,153)
+          ORDER BY t.fecha_registro;
 
 
--- CONTRALORÍA/SISPOA CONAUD/CONAUD/TAREAS/REUTILIZAR COMPONENTE DE REPORTE/REPORTE.md
+++ CONTRALORÍA/SISPOA CONAUD/CONAUD/TAREAS/REUTILIZAR COMPONENTE DE REPORTE/REPORTE.md
@@ -1,23 +1,36 @@
-## Roles
+## Roles **GDC-GAD**
 
-| _list_   | Doc. Identidad | Nombre Completo                               | Rol                | Estado  |
-| -------- | -------------- | --------------------------------------------- | ------------------ | ------- |
-|          | 3403668        | ANA MARIA BRIGGITTE DEL CARMEN ARRAZOLA DURAN | FORMULADOR         | EDICION |
-|          | 2389409        | OLGA EDITH SUAREZ JIMENEZ                     | SUPERVISOR         | EDICION |
-| comision | 4385501        | JULIO CARLOS GUERRA VILLARROEL                | APROBADOR          | EDICION |
-|          | 1397469        | RUTH TARCAYA GALLARDO                         | FORMULADOR GERENTE | EDICION |
-|          | 1397469        | RUTH TARCAYA GALLARDO                         | FORMULADOR         | EDICION |
+| _list_   | Doc. Identidad | Nombre Completo                               | Rol                | per_codigo |
+| -------- | -------------- | --------------------------------------------- | ------------------ | ---------- |
+|          | 3403668        | ANA MARIA BRIGGITTE DEL CARMEN ARRAZOLA DURAN | FORMULADOR         | 190        |
+| comision | 2389409        | OLGA EDITH SUAREZ JIMENEZ                     | SUPERVISOR         | 79         |
+| comision | 4385501        | JULIO CARLOS GUERRA VILLARROEL                | APROBADOR          | 1016       |
+| comision | 1397469        | RUTH TARCAYA GALLARDO                         | FORMULADOR GERENTE | 216        |
+| comision | 1397469        | RUTH TARCAYA GALLARDO                         | FORMULADOR         | 216        |
 ## Query
 ```sql
 SELECT
 		aur.per_codigo, aur.aun_codigo_ejecutora,
-		r.rol_nombre 
+		r.rol_nombre ,
+		au.aun_sigla 
 FROM 	estructura_poa.area_unidad_responsables aur
 		LEFT JOIN parametricas.roles r ON aur.rol_codigo = r.rol_codigo
+		LEFT JOIN estructura_organizacional.areas_unidades au ON aur.aun_codigo_ejecutora = au.aun_codigo 
 WHERE 	TRUE 
 		AND aur.aur_estado IN (1)
 		AND aur.rol_codigo IN (5)
 		AND aur.poa_codigo IN (3)
-		AND aur.per_codigo IN (1016)
+--		AND aur.per_codigo IN (1016)
+--		AND au.aun_sigla LIKE 'GDC-GAD'
+		AND au.aun_codigo IN (18)
+ORDER BY aur.aun_codigo_ejecutora DESC
 ;
 ```
+## Comisión
+- 7881681
+- 7936263
+- 8679431
+- 6526093
+- 4008643
+- 973575
+- 
