9295510f469bff509780bd079f3444cfec6f3659
rteran <richard_teran@contraloria.gob.bo>
2024-12-15 20:34:51 -0400

    Actualizar scripts de DBeaver

--- 6archivos/querys/Script-19.sql
+++ 6archivos/querys/Script-19.sql
@@ -1,115 +1,57 @@
---OBTIENE LOS DETALLES DE F1 que se quedo historico con iapCodigo
-SELECT
-  		a2.act_numero, au.aun_sigla,t.iap_codigo, t.iap_estado, t.iac_codigo , t.act_codigo, t.tia_codigo, t.iap_fecha_aprobacion,
-  		ett.ett_codigo,
-  		iapa.asi_codigo,
-  		a.asi_codigo, a.asi_estado,
-	  (
-	    CASE
-	        WHEN (tt.ett_codigo = 0 and t.tia_codigo = 1) THEN 'REPORTE F1'
-	        WHEN (tt.ett_codigo = 0 and t.tia_codigo = 2) THEN 'REPORTE F1-A'
-	        WHEN (tt.ett_codigo = 1 and t.tia_codigo = 1) THEN 'REPORTE F1'
-	        WHEN (tt.ett_codigo = 1 and t.tia_codigo = 2) THEN 'REPORTE F1-A'
-	       	WHEN (tt.ett_codigo = 2 and t.tia_codigo = 1) THEN 'REPORTE F1'
-	        WHEN (tt.ett_codigo = 2 and t.tia_codigo = 2) THEN 'REPORTE F1-A'
-	       	WHEN (tt.ett_codigo = 3 and t.tia_codigo = 3) THEN 'REPORTE F2'
-	        WHEN (tt.ett_codigo = 3 and t.tia_codigo = 4) THEN 'REPORTE F2-A'
-	       	WHEN (tt.ett_codigo = 4 and t.tia_codigo = 1) THEN 'REPORTE F1'
-	        WHEN (tt.ett_codigo = 4 and t.tia_codigo = 2) THEN 'REPORTE F1-A'
-	       	WHEN (tt.ett_codigo = 5 and t.tia_codigo = 1) THEN 'REPORTE F1'
-	        WHEN (tt.ett_codigo = 5 and t.tia_codigo = 2) THEN 'REPORTE F1-A'
-	        ELSE '' END
-	  ) AS nombre_reporte,
-	  (
-	    CASE
-	        WHEN (tt.ett_codigo = 0 and t.tia_codigo = 1) THEN 1
-	        WHEN (tt.ett_codigo = 0 and t.tia_codigo = 2) THEN 2
-	        WHEN (tt.ett_codigo = 1 and t.tia_codigo = 1) THEN 1
-	        WHEN (tt.ett_codigo = 1 and t.tia_codigo = 2) THEN 2
-	       	WHEN (tt.ett_codigo = 2 and t.tia_codigo = 1) THEN 1
-	        WHEN (tt.ett_codigo = 2 and t.tia_codigo = 2) THEN 2
-	       	WHEN (tt.ett_codigo = 3 and t.tia_codigo = 3) THEN 3
-	        WHEN (tt.ett_codigo = 3 and t.tia_codigo = 4) THEN 1
-	       	WHEN (tt.ett_codigo = 4 and t.tia_codigo = 1) THEN 1
-	        WHEN (tt.ett_codigo = 4 and t.tia_codigo = 2) THEN 2
-	       	WHEN (tt.ett_codigo = 5 and t.tia_codigo = 1) THEN 1
-	        WHEN (tt.ett_codigo = 5 and t.tia_codigo = 2) THEN 2
-	        ELSE 0 END
-	  ) AS tipo_reporte,
-	iapa.asi_codigo 
-FROM ejecucion_actividades.inicio_actividad_poa t
-		LEFT JOIN ejecucion_actividades.inicios_actividades ia on t.iac_codigo = ia.iac_codigo
-		LEFT JOIN parametricas.tipos_trabajos tt on ia.ttr_codigo = tt.ttr_codigo
-		LEFT JOIN parametricas.especificacion_tipos_trabajo ett on tt.ett_codigo = ett.ett_codigo
-		LEFT JOIN estructura_poa.actividades a2 on t.act_codigo = a2.act_codigo
-		LEFT JOIN estructura_organizacional.areas_unidades au ON a2.aun_codigo_ejecutora = au.aun_codigo
-		LEFT JOIN ejecucion_actividades.inicio_actividad_poa_asignaciones iapa on t.iap_codigo = iapa.iap_codigo
-		LEFT JOIN ejecucion_poa.asignaciones a on iapa.asi_codigo = a.asi_codigo
-WHERE	TRUE
-		AND t.iap_estado NOT IN (0)
---		AND tt.ett_codigo IN (2)
-		AND t.iap_estado IN (5)
---		AND ia.iac_codigo IN ()
-		AND t.tia_codigo IN (1)
---		AND t.iap_estado IN (16)
---		AND ia.ttr_codigo IN (0)
---		AND t.iap_codigo IN (70)
---		and t.act_codigo in (1121)
---		and	t.act_codigo = 1448
---		and t.act_codigo = 1200
---		and t.act_codigo = 1181
---		and	t.act_codigo = 1156 --no da
---		and t.act_codigo in (933) 
---ORDER BY t.fecha_registro DESC
+      SELECT  iac.*, ges.ges_anio
+      FROM        ejecucion_actividades.inicios_actividades iac
+              LEFT JOIN parametricas.gestiones ges ON iac.ges_codigo = ges.ges_codigo
+      WHERE     TRUE
+      AND iac.iac_estado IN (22)
+--      AND iac.iac_codigo_control LIKE 'ALCP25M07'
 ;
-
+--$$$$$$$$$$$$$$$$$$$$$$$$$$$
 SELECT 	*
-FROM 	ejecucion_actividades.inicio_actividad_poa iap 
-WHERE 	TRUE
---		AND iap.act_codigo IN (3261)
-		AND iap.iap_codigo IN (695)	
-;
-SELECT 	*
-FROM 	ejecucion_actividades.inicios_actividades ia 
-WHERE 	TRUE 
---		AND ia.iac_codigo IN (623)
-		AND ia.ttr_codigo IN (0)
-;
-
-SELECT 	*
-FROM 	parametricas.tipos_inicios_actividades tia 
-;
-SELECT 	*
-FROM 	ejecucion_poa.asignaciones_cargos_item aci
-WHERE 	TRUE
-		AND aci.persona_detalle NOTNULL 
---		AND aci.persona_detalle ISNULL
---		AND aci.asi_codigo IN (89)
-;
-
-SELECT 	*
-		--a.asi_codigo, a.asi_detalle_asignaciones_cargos_items 
-FROM 	ejecucion_poa.asignaciones a
-WHERE 	TRUE 
-		AND a.asi_codigo IN (637)
---		AND a.asi_detalle_asignaciones_horas_usadas NOTNULL 
-;
---
-SELECT 	
-		au.aun_sigla,
-		ia.iac_codigo, ia.iac_estado,
-		iap.iap_codigo, iap.iap_estado, iap.tia_codigo,
-		a.act_codigo, a.act_estado, a.act_numero 
 FROM 	ejecucion_actividades.inicios_actividades ia
-		LEFT JOIN ejecucion_actividades.inicio_actividad_poa iap ON ia.iac_codigo = iap.iac_codigo 
-		LEFT JOIN estructura_poa.actividades a ON iap.act_codigo = a.act_codigo 
-		LEFT JOIN estructura_organizacional.areas_unidades au ON a.aun_codigo_ejecutora = au.aun_codigo 
-WHERE 	TRUE 
---		AND ia.iac_codigo_control LIKE 'IXDP290Y24'
-		AND ia.iac_codigo_control LIKE 'ECTP346J24'
+WHERE 	TRUE
+--		AND ia.iac_codigo_control LIKE 'ALCP25M07'
+--		AND ia.iac_estado IN (22)
+ORDER BY ia.iac_codigo DESC
 ;
-SELECT	*
-FROM 	ejecucion_actividades.inicio_actividad_poa iap 
-ORDER BY iap.iap_fecha_aprobacion DESC
+--&&&&&&&&&&&&&&&&&&&&&&&&&&&
+SELECT
+		a.act_codigo, a.act_numero, a.iac_codigo_apoyo,a.act_descripcion, a.ttr_codigo, a.tipact_codigo, 
+		tt.ttr_descripcion, ta.tipact_descripcion, ac.iac_codigo, ac.aco_estado 
+FROM 	estructura_poa.actividades a 
+		LEFT JOIN parametricas.tipos_trabajos tt ON a.ttr_codigo = tt.ttr_codigo
+		LEFT JOIN parametricas.tipos_actividades ta ON a.tipact_codigo = ta.tipact_codigo
+		LEFT JOIN estructura_poa.actividades_continuidad ac ON a.act_codigo = ac.act_codigo
+WHERE 	a.fecha_registro::date = CURRENT_DATE
+ORDER BY a.act_codigo DESC
 ;
-;
+
+
+
+
+SELECT 	*
+FROM 	estructura_poa.actividades_continuidad ac ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
